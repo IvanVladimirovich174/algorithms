6778590b9e9ca3cb69c53ebeb31f9d3c1bee1daa
IvanVladimirovich174 <50528755+IvanVladimirovich174@users.noreply.github.com>
2022-10-15 16:20:28 +0300

    Main

--- password/Main.java
+++ password/Main.java
@@ -8,13 +8,15 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int n = scanner.nextInt();
-        char[] password = GenerationPassword.generate(n);
+        char[] password = PasswordGeneration.generate(n);
 
         for (char c : password) {
             System.out.print(c);
         }
 
+        System.out.println();
+
         String passwordString = new String(password);
-        System.out.println(ValidationPassword.validate(passwordString));
+        System.out.println(PasswordValidation.validate(passwordString));
     }
 }
