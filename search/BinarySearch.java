cae9026b7a17038cc7e3fff087573ff25e0a83ad
IvanVladimirovich174 <50528755+IvanVladimirovich174@users.noreply.github.com>
2022-10-25 00:40:39 +0300

    search

---
+++ search/BinarySearch.java
@@ -0,0 +1,19 @@
+package search;
+
+public class BinarySearch {
+    public static int binarySearch(int[] array, int key) {
+        int low = 0;
+        int high = array.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (key < array[mid])
+                high = mid - 1;
+            else if (key == array[mid])
+                return mid;
+            else
+                low = mid + 1;
+        }
+        return -(low + 1);
+    }
+}
---
+++ search/RandomArray.java
@@ -0,0 +1,14 @@
+package search;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomArray {
+    public static int[] createRandomArray(int n) {
+        int[] m = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            m[i] = ThreadLocalRandom.current().nextInt(0, 1001);
+        }
+        return m;
+    }
+}
