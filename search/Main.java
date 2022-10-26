9896035a154178876e25a98b61e685bec464db62
IvanVladimirovich174 <50528755+IvanVladimirovich174@users.noreply.github.com>
2022-10-26 23:47:19 +0300

    Main

--- search/Main.java
+++ search/Main.java
@@ -1,6 +1,6 @@
 package search;
 
-import sort.SelectionSort;
+import sort.QuickSort;
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -11,16 +11,20 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
 
-        //binary search
-        int[] binarySearch = RandomArray.createRandomArray(n);
-        System.out.println("Первоначальный массив: " + Arrays.toString(binarySearch));
-        SelectionSort.sort(binarySearch);
-        System.out.println("Отсортированный массив" + Arrays.toString(binarySearch));
+        int[] arrayToSearch = RandomArray.createRandomArray(n);
+        System.out.println("Первоначальный массив: " + Arrays.toString(arrayToSearch));
+        QuickSort.sort(arrayToSearch);
+        System.out.println("Отсортированный массив" + Arrays.toString(arrayToSearch));
 
         System.out.print("Введите целое число, которое необходимо найти: ");
         int key = scanner.nextInt();
 
-        int keyPosition = BinarySearch.binarySearch(binarySearch, key);
-        System.out.println("Индекс нахождения элемента в массиве: " + keyPosition);
+        //LinearSearch
+        int keyPositionLinearSearch = LinearSearch.linearSearch(arrayToSearch, key);
+        System.out.println("[Линейный поиск]Индекс нахождения элемента в массиве: " + keyPositionLinearSearch);
+
+        //BinarySearch
+        int keyPositionBinarySearch = BinarySearch.binarySearch(arrayToSearch, key);
+        System.out.println("[Бинарный поиск]Индекс нахождения элемента в массиве: " + keyPositionBinarySearch);
     }
 }
