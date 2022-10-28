a9fb9661c827d58a23c0b0a26f53f44ffd85a4a9
IvanVladimirovich174 <50528755+IvanVladimirovich174@users.noreply.github.com>
2022-10-28 17:30:43 +0300

    sort

--- sort/BubbleSort.java
+++ sort/BubbleSort.java
@@ -1,13 +1,32 @@
 package sort;
 
 public class BubbleSort {
-    public static void sort(int[] array) {
+    public static void ascendingSort(int[] array) {
         boolean isSorted;
 
         for (int i = 0; i < array.length - 1; i++) {
             isSorted = true;
             for (int j = 0; j < array.length - 1 - i; j++) {
                 if (array[j] > array[j + 1]) {
+                    int tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                    isSorted = false;
+                }
+            }
+            if (isSorted) {
+                break;
+            }
+        }
+    }
+
+    public static void desccendingSort(int[] array) {
+        boolean isSorted;
+
+        for (int i = 0; i < array.length - 1; i++) {
+            isSorted = true;
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] < array[j + 1]) {
                     int tmp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = tmp;
